8e635843ac3520c5b12de8f378ece3a4e33cb104
Natnaka Sundaranaga <natnaka@gmail.com>
2018-05-05 09:23:39 +0700

    remove all unnecessary config

--- config/default.py
+++ config/default.py
@@ -9,16 +9,10 @@ class Config(object):
     DEBUG = False
     TESTING = False
 
-    SECRET_KEY = "\xa3\xbfN\xf3T\xd6\xa1S\xf6y\xd2@\xccnn\x87R\x9fNY\xb5^\xf2\xf8"
-    # PERMANENT_SESSION_LIFETIME = 3600 * 24 * 7
-    # SESSION_COOKIE_NAME = 'scb_abacus_session'
+    SECRET_KEY = "abcdef12345"
 
     # Root path of project
     PROJECT_PATH = PROJECT_PATH
-
-    # Site domain
-    # SITE_TITLE = "Pali Dictionary"
-    # SITE_DOMAIN = "http://localhost:8000"
 
     # SQLAlchemy config
     # See:
@@ -27,31 +21,8 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://user:password@host/database"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Flask Security
-    # https://pythonhosted.org/Flask-Security/configuration.html
-    # SECURITY_PASSWORD_HASH
-    # SECURITY_REGISTERABLE = True
-    # SECURITY_SEND_REGISTER_EMAIL = False
-    # SECURITY_SEND_PASSWORD_CHANGE_EMAIL = False
-    # SECURITY_LOGIN_WITHOUT_CONFIRMATION = False
-    # SECURITY_PASSWORD_SALT = '\xc7F\xcce\x03\xc3\x02_\x04\x1e@\x9fC\xf7'
-
-    # JWT
-
-    # FCM
-    # PARTY SERVICES
-
-    # Security
-
     # Flask-DebugToolbar
     DEBUG_TB_INTERCEPT_REDIRECTS = False
 
     # Sentry config
     JSON_AS_ASCII = False
-
-    # Host string, used by fabric
-
-    # Audit
-    #AUDIT_PATH = "path/to/audit"
-    #AUDIT_MAX_FILE_SIZE = 4*1024*1024  # 4 GB
-    #AUDIT_MAX_FILE_COUNT = 1000        # allow split to maximum 1000 files
