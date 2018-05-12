8aaaf5df0d137c48e40f5998a96234e5b02bdf5b
Natnaka Sundaranaga <natnaka@gmail.com>
2018-05-12 18:10:42 +0700

    similar words, when not found exact word

--- application/controllers/pali_meaning.py
+++ application/controllers/pali_meaning.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
-from flask import Blueprint, jsonify
+import math
+from flask import Blueprint, jsonify, request
 from application.models import TwarePaliWord
 from application.models import ThePali
 
@@ -7,6 +8,9 @@ bp = Blueprint('pali_controller', __name__, url_prefix='/pali')
 
 @bp.route('/meaning/<word>', methods=['GET'])
 def meaning(word):
+    similar = int(request.args.get('similarity', 60))/100.0
+    print("similar = ", similar)
+
     r = TwarePaliWord.query.filter_by(word=word).first()
     d = dict(success=False)
     if r:
@@ -26,5 +30,20 @@ def meaning(word):
                 meaning[col.name] = getattr(r, col.name)
             means.append(meaning)
         d['the_pali_meaning'] = means
+
+    if not d['success']:
+        # Find similar word
+        n = int(math.ceil(len(word) * similar))
+        prefix = word[:n]
+        print("Find similar word! [%s]" %prefix)
+        rs = TwarePaliWord.query.filter(TwarePaliWord.word.startswith(prefix)).all()
+        
+        means = []
+        for r in rs:
+            meaning = {}
+            for col in r.__table__.columns:
+                meaning[col.name] = getattr(r, col.name)
+            means.append(meaning)
+        d['similar_words'] = means
         
     return jsonify(d)
